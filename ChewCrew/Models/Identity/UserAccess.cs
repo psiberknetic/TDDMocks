48cc475c39cc5fde08505e7e1ba06c023cacb632
Nate Adams <psiberknetic@gmail.com>
2019-11-20 09:55:51 -0600

    Iniital implementation of UserAccess

--- ChewCrew/Models/Identity/UserAccess.cs
+++ ChewCrew/Models/Identity/UserAccess.cs
@@ -1,11 +1,45 @@
-﻿using System;
+﻿using ChewCrew.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
 
 namespace ChewCrew.Models.Identity
 {
-    public class UserAccess
+    public class UserAccess : IUserAccess
     {
+        private readonly IPrincipal _principal;
+
+        public UserAccess(IPrincipal principal)
+        {
+            _principal = principal;
+        }
+
+        public bool CanAddRestaurant()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanEliminateRestaurantSuggestions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanOverrideGroupDecision()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanRateRestaurant()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanSuggestRestaurants()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
